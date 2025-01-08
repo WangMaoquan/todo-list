09f1426dad174fcda6c63af2d14ff8759d950136
wangmaoquan <1782976211@qq.com>
2025-01-08 10:39:59 +0800

    refactor: 统一代码格式

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  {
+    ignores: [
+      'node_modules/**',
+      '**/dist/**',
+      '*.yml',
+      '*.yaml',
+      '*.json',
+      '*.md',
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      semi: 'error',
+    },
+  },
+];
