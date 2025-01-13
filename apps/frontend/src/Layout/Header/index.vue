298c8acb208e01da0c43d3ee48a547989010e2b7
wangmaoquan <1782976211@qq.com>
2025-01-13 14:28:09 +0800

    feat: header navbar

--- apps/frontend/src/Layout/Header/index.vue
+++ apps/frontend/src/Layout/Header/index.vue
@@ -1,7 +1,23 @@
+<script setup lang="ts">
+import { openGithub, useGoto } from '@/composables/goto';
+
+const { gotoHome, gotoSettings } = useGoto();
+</script>
+
 <template>
-  <div>header</div>
+  <div
+    class="w-full base-color h-40px px-1% flex justify-between items-center text-16px"
+  >
+    <div class="cursor-pointer" aria-label="Go Home" @click="gotoHome()">
+      TODO-LIST
+    </div>
+    <div class="flex items-center justify-start">
+      <button class="mx-2 !outline-none" @click="gotoSettings()">
+        <div i="carbon-settings" />
+      </button>
+      <button class="mx-2 !outline-none" @click="openGithub()">
+        <div i="mdi-github" />
+      </button>
+    </div>
+  </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
--- apps/frontend/src/Layout/Navbar/index.vue
+++ apps/frontend/src/Layout/Navbar/index.vue
@@ -1,7 +1,76 @@
+<script setup lang="ts">
+import { NDropdown, NPopover } from 'naive-ui';
+import type { DropdownMixedOption } from 'naive-ui/es/dropdown/src/interface';
+import { useCommandModal } from '@/components/command/commandModal';
+import Command from '@/components/command/CommandModal.vue';
+import { useGoto } from '@/composables/goto';
+
+const { gotoSettings } = useGoto();
+const { openCommandModal } = useCommandModal();
+
+const settingOptions: DropdownMixedOption[] = [
+  {
+    label: '设置',
+    key: 'settings',
+    props: {
+      onClick: () => {
+        gotoSettings();
+      },
+    },
+  },
+];
+</script>
+
 <template>
-  <div>navbar</div>
+  <div class="w-[48px] border-r border-blue border-solid py-2 dark:bg-black">
+    <NDropdown placement="right-end" :options="settingOptions">
+      <!-- avatar -->
+      <div class="w-[32px] h-[32px] rounded-sm bg-blue mx-auto">
+        <img src="" alt="" />
+      </div>
+    </NDropdown>
+    <!-- page router -->
+    <div
+      class="text-gray-4 flex flex-col justify-center items-center text-xl mt-10 gap-6 font-bold"
+    >
+      <NPopover placement="right">
+        <template #trigger>
+          <div
+            class="hover:text-gray-6 dark:hover:text-gray-2"
+            i="carbon-checkbox-checked-filled"
+          />
+        </template>
+        <span>任务</span>
+      </NPopover>
+      <NPopover placement="right">
+        <template #trigger>
+          <div
+            class="hover:text-gray-6 dark:hover:text-gray-2"
+            i="carbon-calendar-heat-map"
+          />
+        </template>
+        <span>日历</span>
+      </NPopover>
+      <NPopover placement="right">
+        <template #trigger>
+          <div
+            class="hover:text-gray-6 dark:hover:text-gray-2"
+            i="carbon-task-complete"
+          />
+        </template>
+        <span>打卡</span>
+      </NPopover>
+      <NPopover placement="right">
+        <template #trigger>
+          <div
+            class="hover:text-gray-6 dark:hover:text-gray-2 cursor-pointer"
+            i="carbon-search"
+            @click="() => openCommandModal()"
+          />
+        </template>
+        <span>搜索面板</span>
+      </NPopover>
+    </div>
+    <Command />
+  </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
