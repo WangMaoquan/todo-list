852a59577c128fe41928eca851cdb5d2e9e2f646
wangmaoquan <1782976211@qq.com>
2025-01-13 09:34:27 +0800

    feat(frontend): unocss vite vitest 配置

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite';
+import path from 'node:path';
+import { defineConfig, ViteUserConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import Unocss from 'unocss/vite';
 
-// https://vite.dev/config/
+const plugins = [vueJsx(), vue(), Unocss()] as ViteUserConfig['plugins'];
+
 export default defineConfig({
-  plugins: [vue()],
+  test: {
+    environment: 'happy-dom',
+    setupFiles: path.resolve(__dirname, './vitest.setup.ts'),
+  },
+  plugins,
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+      'services/': `${path.resolve(__dirname, 'src/services')}/`,
+    },
+  },
 });
