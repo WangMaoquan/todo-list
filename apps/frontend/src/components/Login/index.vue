a2c0408f17d839caf61f585705c80c3ebcb86f5d
wangmaoquan <1782976211@qq.com>
2025-01-13 14:24:15 +0800

    feat: 登录注册组件

--- apps/frontend/src/components/Login/index.vue
+++ apps/frontend/src/components/Login/index.vue
@@ -1,7 +1,45 @@
+<script setup lang="ts">
+import { NCard, NTabPane, NTabs } from 'naive-ui';
+import SignUp from './SignUp.vue';
+import SignIn from './SignIn.vue';
+</script>
+
 <template>
-  <div>login</div>
+  <div>
+    <NCard>
+      <NTabs
+        class="card-tabs"
+        default-value="signIn"
+        size="large"
+        animated
+        pane-wrapper-style="margin: 0 -4px"
+        pane-style="padding-left: 4px; padding-right: 4px; box-sizing: border-box;"
+      >
+        <NTabPane name="signIn" tab="登录">
+          <SignIn />
+        </NTabPane>
+        <NTabPane name="signup" tab="注册">
+          <SignUp />
+        </NTabPane>
+      </NTabs>
+    </NCard>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<style>
+body {
+  @apply dark bg-black text-white;
+}
 
-<style scoped></style>
+.h-full {
+  height: 100vh;
+}
+
+.n-card {
+  width: 400px;
+}
+
+.card-tabs .n-tabs-nav--bar-type {
+  padding-left: 4px;
+}
+</style>
