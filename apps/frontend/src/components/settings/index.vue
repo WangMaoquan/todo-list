e6050e0191b029e52bd2b01f5c9ce8362ac46161
wangmaoquan <1782976211@qq.com>
2025-01-13 17:32:21 +0800

    feat: 设置页面

---
+++ apps/frontend/src/components/settings/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SettingsBody from './SettingsBody.vue';
+import SettingsHeader from './SettingsHeader.vue';
+</script>
+
+<template>
+  <div class="base-color h-[calc(100vh-40px)]">
+    <div class="w-1000px my-0 mx-auto">
+      <SettingsHeader />
+      <SettingsBody class="mt-20px" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
