0e5f1677096e95b2775338d3e103bd36aeb8a6b7
wangmaoquan <1782976211@qq.com>
2025-01-13 14:22:56 +0800

    feat(frontend): 新增 command 相关组件以及store

---
+++ apps/frontend/src/components/command/CommandModal.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { NModal } from 'naive-ui';
+import { watch } from 'vue';
+import { useSearch, useCommandModal } from '@/composables/commamd';
+import CommandBody from './CommandModalBody.vue';
+
+const { resetSearch } = useSearch();
+const { registerKeyboardShortcut, showCommandModal } = useCommandModal();
+
+registerKeyboardShortcut();
+
+watch(
+  () => showCommandModal.value,
+  (v) => {
+    if (!v) resetSearch();
+  },
+);
+</script>
+
+<template>
+  <NModal v-model:show="showCommandModal" display-directive="show">
+    <CommandBody />
+  </NModal>
+</template>
+
+<style scoped></style>
---
+++ apps/frontend/src/components/command/CommandSearchCommands.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useSearchCommands, useCommandModal } from '@/composables/commamd';
+import type { Command } from '@/composables/commamd';
+
+const { closeCommandModal } = useCommandModal();
+const { filteredCommands } = useSearchCommands();
+
+const handleClick = (command: Command) => {
+  command.execute();
+  closeCommandModal();
+};
+</script>
+
+<template>
+  <div>
+    <div
+      v-for="(item, key) in filteredCommands"
+      :key="key"
+      class="hover:bg-gray-400/5 leading-30px px-2 cursor-pointer"
+      @click="handleClick(item)"
+    >
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
